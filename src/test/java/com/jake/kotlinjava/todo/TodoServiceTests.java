5274f582cd75124e21dc6c02c7f23de0e1632e7d
yklee <gerion@gmail.com>
2023-08-15 16:11:08 +0900

    Service Test 추가

---
+++ src/test/java/com/jake/kotlinjava/todo/TodoServiceTests.java
@@ -0,0 +1,57 @@
+package com.jake.kotlinjava.todo;
+
+import com.jake.kotlinjava.todo.domain.Todo;
+import com.jake.kotlinjava.todo.domain.TodoRepository;
+import com.jake.kotlinjava.todo.service.TodoService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(SpringExtension.class)
+public class TodoServiceTests {
+
+	@MockBean
+	private TodoRepository todoRepo;
+
+	private TodoService todoService;
+
+	private Todo stub;
+
+	@BeforeEach
+	public void setUp() {
+		todoService = new TodoService(todoRepo);
+		stub = todoStub();
+	}
+
+	@Test
+	public void 한개의_TODO_반환해야함() {
+		// Given
+		given(todoRepo.findById(1L)).willReturn(Optional.of(stub));
+
+		// When
+		Todo actual = todoService.findById(1L);
+
+		// Then
+		assertThat(actual).isNotNull();
+		assertThat(actual).isEqualTo(stub);
+	}
+
+	public Todo todoStub() {
+		return Todo.builder()
+				.id(1L)
+				.title("테스트")
+				.description("테스트 상세")
+				.done(false)
+				.createdAt(LocalDateTime.now())
+				.updatedAt(LocalDateTime.now())
+				.build();
+	}
+}
