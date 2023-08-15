eae6b7455677138e62e9b747a377aae75c082129
yklee <gerion@gmail.com>
2023-08-15 15:22:37 +0900

    Initial commit

---
+++ src/main/java/com/jake/todo/java/TodoJavaSampleApplication.java
@@ -0,0 +1,13 @@
+package com.jake.todo.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoJavaSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoJavaSampleApplication.class, args);
+	}
+
+}
